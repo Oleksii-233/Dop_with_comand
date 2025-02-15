912a0d481244b9bb82d29f52f268bbc9ce8b17be
Oleksii-233 <bashuk0325oleksij@gmail.com>
2025-02-15 12:33:58 +0200

    Blok1 - ended (so -so)

--- Dop_with_comand/Blok1.cpp
+++ Dop_with_comand/Blok1.cpp
@@ -1,5 +1,7 @@
 #include "Blok1.h"
 
+#include "Blok2.h"
+
 int GetCountFromFile(fstream& file, student* s) {
 	student stud;
 	int k = 0;
@@ -39,4 +41,125 @@ void Smallest(fstream& file) {
 		if (students[i].aver == min)
 			ShowOne(students[i]);
 	
+}
+
+void CalcucaleAverInGroup(Group& g) {
+
+	for (int i = 0; i < g.k; i++) {
+		g.aM += g.s[i].math;
+		g.aP += g.s[i].phisic;
+		g.aE += g.s[i].english;
+		g.aU += g.s[i].ukrainian;
+	}
+
+	g.aM /= g.k;
+	g.aP /= g.k;
+	g.aE /= g.k;
+	g.aU /= g.k;
+
+}
+
+void Math_s(Group* p, int s) {
+
+	for (int i = 0; i < s - 1; i++)
+		for (int j = i + 1; j < s; j++)
+			if (p[i].aM > p[j].aM)
+				swap(p[i], p[j]);
+
+}
+
+void Phicics_s(Group* p, int s) {
+
+	for (int i = 0; i < s - 1; i++)
+		for (int j = i + 1; j < s; j++)
+			if (p[i].aP > p[j].aP)
+				swap(p[i], p[j]);
+
+}
+
+void English_s(Group* p, int s) {
+
+	for (int i = 0; i < s - 1; i++)
+		for (int j = i + 1; j < s; j++)
+			if (p[i].aE > p[j].aE)
+				swap(p[i], p[j]);
+
+}
+
+void Ukrainian_s(Group* p, int s) {
+
+	for (int i = 0; i < s - 1; i++)
+		for (int j = i + 1; j < s; j++)
+			if (p[i].aU > p[j].aU)
+				swap(p[i], p[j]);
+
+}
+
+
+void ShowArrGroup(Group* p, int s) {
+	for (int i = 0; i < s; i++)
+		cout << p[i].name;
+}
+
+void Reyt(fstream& file) {
+
+	IsOpen(file);
+
+	if (IsEmpty(file)) {
+		cout << "File is empty." << endl;
+		return;
+	}
+
+	student s[15]; 
+	int k = 0;
+	Group g[15];
+
+	k = GetCountFromFile(file, s);
+
+	strcpy(g[0].name, s[0].group);
+	g[0].s[g[0].k++] = s[0];
+
+	int size = 1;
+	
+	// Розподіл на групи
+	for (int i = 1; i < k; i++) {
+		bool find = false;
+
+		for (int j = 0; j < size; j++) {
+
+			if (strcmp(g[j].name, s[i].group) == 0) {
+				find = true;
+				g[j].s[g[j].k++] = s[i];
+				break;
+			}
+
+		}
+
+		if (!find) {
+			g[size].s[g[size].k++] = s[i];
+			strcpy(g[size++].name, s[i].group);
+		}
+
+
+	}
+
+	for (int i = 0; i < size; i++)
+		CalcucaleAverInGroup(g[i]);
+	
+	cout << "Reyt for math: ";
+	Math_s(g, size);
+	ShowArrGroup(g, size);
+
+	cout << "Reyt for phicics: ";
+	Phicics_s(g, size);
+	ShowArrGroup(g, size);
+
+	cout << "Reyt for english: ";
+	English_s(g, size);
+	ShowArrGroup(g, size);
+
+	cout << "Reyt for ukrainian: ";
+	Ukrainian_s(g, size);
+	ShowArrGroup(g, size);
+
 }
--- Dop_with_comand/Blok1.h
+++ Dop_with_comand/Blok1.h
@@ -6,9 +6,30 @@
 #include "File_blok.h"
 #include "Blok0.h"
 
+struct Group {
+	char name[10] = "";
+	student s[15];
+	int k = 0;
+
+	double aM = 0, aP = 0, aE = 0, aU = 0;
+};
+
 void Smallest(fstream& file);
 
 int GetCountFromFile(fstream& file, student* s);
 
+void CalcucaleAverInGroup(Group& g);
+
+void Math_s(Group* p, int s);
+
+void Phicics_s(Group* p, int s);
+
+void English_s(Group* p, int s);
+
+void Ukrainian_s(Group* p, int s);
+
+void ShowArrGroup(Group* p, int s);
+
+void Reyt(fstream& file);
 
 #endif
